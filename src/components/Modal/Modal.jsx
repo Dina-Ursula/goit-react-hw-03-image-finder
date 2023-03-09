8287fe978b50476f043736fa791eb63e4972b429
Dina-Ursula <dinatyan26@gmail.com>
2023-03-10 00:58:01 +0200

    update/npx/caniuse

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,15 +1,6 @@
 import React, { Component } from 'react';
 import { Overlay, ModalStyled } from './Modal.styled';
 
-// const Modal = ({ image, closeModal }) => {
-//   return (
-//     <Overlay onClick={closeModal}>
-//       <ModalStyled class="modal">
-//         <img src={image} />
-//       </ModalStyled>
-//     </Overlay>
-//   );
-// };
 class Modal extends Component {
   componentDidMount() {
     window.addEventListener('keydown', e => {
